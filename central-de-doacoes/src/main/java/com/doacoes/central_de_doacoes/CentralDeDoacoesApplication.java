51d75c9e66e26177fb2634f720ce080b0c7736a9
Viniciuseel <125770820+Viniciuseel@users.noreply.github.com>
2024-10-11 13:30:40 -0300

    Definir a estrutura de diretorios

---
+++ central-de-doacoes/src/main/java/com/doacoes/central_de_doacoes/CentralDeDoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.doacoes.central_de_doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeDoacoesApplication.class, args);
+	}
+
+}
---
+++ central-de-doacoes/src/test/java/com/doacoes/central_de_doacoes/CentralDeDoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doacoes.central_de_doacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeDoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
