56d0c64157683ab407da8904a43032511bdf71a9
Vinicius Arruda Ferreira <vinicius.arruda15eel@gmail.com>
2024-10-21 12:17:29 -0300

    Fazer Merge da Development na main

---
+++ src/main/java/com/doacoes/central/CentralDeDoacoesApplication.java
@@ -0,0 +1,14 @@
+package com.doacoes.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeDoacoesApplication.class, args);
+		System.out.println("Ola mundo novamente.");
+	}
+
+}
